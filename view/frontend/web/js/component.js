866c9e7686d1f02a5eff04ee393d578f249c5825
suraj <sprajapat770@gmail.com>
2021-01-04 14:26:39 +0530

    first commit

--- view/frontend/web/js/component.js
+++ view/frontend/web/js/component.js
@@ -1,4 +1,16 @@
-define(['uiElement'],function(Component){
+define(['uiComponent','ko'],function(Component,ko){
 	'use strict';
-	return Component.extend();
+	return Component.extend({
+        _currentTime: ko.observable("Loading...."),//set Initial Value
+        initialize:function () {
+            this._super();
+            setInterval(this.updateTime.bind(this),1000);//update time every second
+        },
+        getTime:function (){
+            return this._currentTime;
+        },
+        updateTime:function (){
+            this._currentTime(new Date());
+        }
+    });
 });
